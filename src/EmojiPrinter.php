b8a687874d60dabd7b4a56d0b8b6f9259ebf617f
Yitzchok Willroth <coderabbi@gmail.com>
2017-07-07 07:46:15 -0400

    :hammer:Extract refactor EmojiPrinter methods to reduce complexity.

--- src/EmojiPrinter.php
+++ src/EmojiPrinter.php
@@ -1,7 +1,8 @@
-<?php declare(strict_types=1);
+<?php declare(strict_types = 1);
 
 namespace Coderabbi\Phpumoji;
 
+use DOMElement;
 use LitEmoji\LitEmoji;
 use PHPUnit\Framework\TestSuite;
 use PHPUnit\TextUI\ResultPrinter;
@@ -9,18 +10,19 @@ use PHPUnit\Util\Xml;
 
 final class EmojiPrinter extends ResultPrinter
 {
+    const CONFIG = __DIR__ . '/../config/';
+    const EMOJIFILE = '.emojifile';
+    const EMOJISET = 'phpumoji';
+    const SPACER = ' ';
+
     private $emojis;
 
     public function startTestSuite(TestSuite $suite)
     {
-        $phpunit = Xml::loadFile($GLOBALS['__PHPUNIT_CONFIGURATION_FILE'], false, true, true)->documentElement;
-
-        $emojiset = $phpunit->hasAttribute('emojiset') ? $phpunit->getAttribute('emojiset') : 'phpumoji';
-
-        $emojifile = array_merge(parse_ini_file(__DIR__ . '/../config/.emojifile', true), file_exists(substr($GLOBALS['__PHPUNIT_CONFIGURATION_FILE'], 0, strrpos($GLOBALS['__PHPUNIT_CONFIGURATION_FILE'], '/')) . '/.emojifile') ?
-            parse_ini_file(substr($GLOBALS['__PHPUNIT_CONFIGURATION_FILE'], 0, strrpos($GLOBALS['__PHPUNIT_CONFIGURATION_FILE'], '/')) . '/.emojifile', true) : []);
-
-        $this->emojis = $emojifile[$emojiset] ?? $emojifile['phpumoji'];
+        $this->emojis = $this->emojis(
+            $this->emojifile($this->projectroot()),
+            $this->emojiset($this->phpunitxml())
+        );
 
         if ($this->numTests == -1) {
             $this->numTests = count($suite);
@@ -33,7 +35,7 @@ final class EmojiPrinter extends ResultPrinter
 
     protected function writeProgress($progress)
     {
-        return parent::writeProgress($this->emojify($progress));
+        return parent::writeProgress($this->emojify($progress) . self::SPACER);
     }
 
     protected function writeProgressWithColor($color, $progress)
@@ -41,10 +43,50 @@ final class EmojiPrinter extends ResultPrinter
         return $this->writeProgress($progress);
     }
 
-    private function emojify(string $progress) :string
+    private function projectroot(): string
     {
-        return LitEmoji::encodeUnicode(':' . (array_values(array_filter($this->emojis, function ($key) use ($progress) {
-            return strrpos(strtoupper($key), $progress) === 0;
-        }, ARRAY_FILTER_USE_KEY))[0] ?? $this->emojis['pass']) . ': ') ;
+        return substr($GLOBALS['__PHPUNIT_CONFIGURATION_FILE'], 0, strrpos(
+            $GLOBALS['__PHPUNIT_CONFIGURATION_FILE'], '/')
+        );
+    }
+
+    private function emojifile(string $projectroot): array
+    {
+        return array_merge(
+            $this->parse(self::CONFIG . self::EMOJIFILE),
+            $this->parse($projectroot . self::EMOJIFILE)
+        );
+    }
+
+    private function phpunitxml(): DOMElement
+    {
+        return Xml::loadFile($GLOBALS['__PHPUNIT_CONFIGURATION_FILE'], false, true, true)->documentElement;
+    }
+
+    private function parse(string $filepath): array
+    {
+        return file_exists($filepath) ? parse_ini_file($filepath, true) : [];
+    }
+
+    private function emojiset(DOMElement $phpunitxml): string
+    {
+        return $phpunitxml->hasAttribute('emojiset') ? $phpunitxml->getAttribute('emojiset') : self::EMOJISET;
+    }
+
+    private function emojis(array $emojifile, string $emojiset): array
+    {
+        return $emojifile[$emojiset] ?? $emojifile[self::EMOJISET];
+    }
+
+    private function emojify(string $result): string
+    {
+        return LitEmoji::encodeUnicode(":{$this->shortcode($result)}:") ;
+    }
+
+    private function shortcode(string $character): string
+    {
+        return array_values(array_filter($this->emojis, function ($emojikey) use ($character) {
+            return strrpos(strtoupper($emojikey), $character) === 0;
+        }, ARRAY_FILTER_USE_KEY))[0] ?? $this->emojis['pass'];
     }
 }
